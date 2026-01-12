7fc612ef8e56a1eb0292da22fb490cca5c3263a2
lamou <69391156+lamouu@users.noreply.github.com>
2026-01-12 17:43:40 +0000

    scaling and minor fixes

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,7 +2,7 @@ extends CharacterBody3D
 
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
-const SENSITIVITY = 0.003
+const SENSITIVITY = 0.001
 
 @onready var head = $Head
 @onready var camera = $Head/Camera3D
